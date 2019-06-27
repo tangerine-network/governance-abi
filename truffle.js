fae78fb230b1b7e508aeaa07dde6d6b53ccbdab3
Wei-Ning Huang <w@byzantine-lab.io>
2019-06-27 14:58:38 +0800

    config: add tangerine network config

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 /*
  * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
@@ -19,6 +21,20 @@ module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
+    "tangerine-mainnet": {
+      network_id: 373,
+      gasPrice: 24e9,
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://mainnet-rpc.tangerine-network.io");
+      },
+    },
+    "tangerine-testnet": {
+      network_id: 374,
+      gasPrice: 24e9,
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://testnet-rpc.tangerine-network.io");
+      },
+    },
     mainnet: {
       network_id: 1,
       gas: 4000000,
